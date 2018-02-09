e4ab6f048c0b69449feed1e5cdf20e9580ee1c3c
hongjunwu <hongjunwu@hotmail.com>
2018-02-08 23:11:05 -0800

    +1s

--- HRL/01-Intro to Python/04-formatOutput.py
+++ HRL/01-Intro to Python/04-formatOutput.py
@@ -3,3 +3,18 @@ Hongjun Wu
 20180208
 This program will test on how to use % to format output.
 """
+name = "Zemin Jiang"
+print('My name is %s, nice to meet you!' % name)
+
+age = 91
+print('My age is %d, you are too young!' % age)
+
+tankman = 1
+print('The number of tankman is %06d' % tankman)
+
+journalist = 1.0
+naive = 99.0
+angry = journalist + naive
+print('There is %.1f journalist and her naive status is %.2f so I am %.3f percent angry.' % (journalist, naive, angry))
+print('You hear me? I am %.02f%% angry!!!' % angry)
+
